da6ef728a8a1a8dc693f80fa71061b1df5fb3aee
unknown <tarrmmy@gmail.com>
2023-06-13 18:59:34 +0100

    50% home page

---
+++ src/components/home/Data.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Data = () => {
+  return (
+    <div className='home__data'>
+        <h1 className="home__title">Ibraheem Qomorudeen</h1>
+        <h3 className="home__subtitle">Frontend Developer</h3>
+        <p className="home__description">
+            rhwsi sksiek seeke workw skjjwi skisao wkwkj jsal ehjqi
+        </p>
+        <a href="#contact" className="button button--flex">
+            Say Hello
+        </a>
+    </div>
+  )
+}
+
+export default Data
