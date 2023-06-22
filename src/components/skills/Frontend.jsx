f4d3da420e28db6cd3c80b0a5f3002372076d97b
unknown <tarrmmy@gmail.com>
2023-06-22 10:53:40 +0100

    services page

--- src/components/skills/Frontend.jsx
+++ src/components/skills/Frontend.jsx
@@ -36,7 +36,7 @@ const Frontend = () => {
                 <div className="skills__data">
                     <i class='bx bx-badge-check' ></i>
                     <div>
-                        <h3 className="skills__name">Tailwind CSS</h3>
+                        <h3 className="skills__name">Tailwind</h3>
                         <span className="skills__level">Intermediate</span>
                     </div>
                 </div>
@@ -52,7 +52,7 @@ const Frontend = () => {
                 <div className="skills__data">
                     <i class='bx bx-badge-check' ></i>
                     <div>
-                        <h3 className="skills__name">React Js</h3>
+                        <h3 className="skills__name">React </h3>
                         <span className="skills__level">Intermediate</span>
                     </div>
                 </div>
