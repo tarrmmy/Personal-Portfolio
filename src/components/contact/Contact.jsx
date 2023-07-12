0554a3a0ff41c61ef722b97490f463d2910de163
unknown <tarrmmy@gmail.com>
2023-07-12 12:17:12 +0100

    updates

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,16 @@ import './contact.css'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <section className="contact section" id='#contact'>
+        <h2 className="section__title">Contact</h2>
+        <span className="section__subtitle">How to reach me</span>
+
+        <div className="contact__container container grid">
+            <div className="contact content">
+                <h3 className="contact__title">Contact</h3>
+            </div>
+        </div>
+    </section>
   )
 }
 
