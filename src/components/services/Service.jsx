5fa06bc4c45db0f710217b25850d0e2d4947fdea
Ibraheem Qomorudeen <85225937+tarrmmy@users.noreply.github.com>
2023-08-19 17:41:21 +0100

    Ibraheem (#17)
    * updates
    
    * updates
    
    * responsiveness
    
    * skills update
    
    * updates

--- src/components/services/Service.jsx
+++ src/components/services/Service.jsx
@@ -11,13 +11,13 @@ const Service = () => {
   return (
     <section className="servies section" id="services">
         <h2 className="section__title">Services</h2>
-        <span className="section__subtitle">My Services</span>
+        <span className="section__subtitle">What I offer in terms of services</span>
 
         <div className="services__container container grid">
             <div className="services__content">
                 <div>
                     <i className="uil uil-web-grid services__icon"></i>
-                    <h3 className="services__title"> Web <br /> Development</h3>
+                    <h3 className="services__title"> Web<br />Development</h3>
                 </div>
 
                 <span className="services__button" onClick= {() => toggleTab(1)}>
@@ -28,7 +28,7 @@ const Service = () => {
                 <div className={toggleState === 1 ? "services__modal active-modal" : "services__modal"}>
                     <div className="services__modal-content">
                         <i className="uil uil-times services__modal-close" onClick={() => toggleTab(0)} ></i>
-                        <h3 className="services__modal-title">Product Designer</h3>
+                        <h3 className="services__modal-title">Web Developer</h3>
                         <p className="services__modal-description">
                             Non harum sequi et amet suscipit eum rerum mollitia. Cum dolore placeat quo accusantium
                         </p>
